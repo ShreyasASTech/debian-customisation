8e07bd5b8976878a7d1d0351204db586d2774667
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2025-01-26 22:24:55 +0530

    Some minor formatting changes

--- scripts/common/install-neovim.sh
+++ scripts/common/install-neovim.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 # Display title of script
-if type _printtitle &> /dev/null; then
+if type _printtitle &>/dev/null; then
   _printtitle "INSTALLING - NEOVIM"
 fi
 
 # Change directory
-SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
+SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$0")") && cd "$SCRIPT_DIR" || exit
 
 # Install dependencies
-if command -v apt-get > /dev/null; then # Install for debian-based distros
+if command -v apt-get >/dev/null; then # Install for debian-based distros
   sudo apt-get install -y gcc ripgrep xsel fd-find python3-venv npm
   sudo ln -sf "$(which fdfind)" "$(dirname "$(which fdfind)")/fd" # Symlink fd -> fdfind
-elif command -v pacman > /dev/null; then # Install for archlinux-based distros
+elif command -v pacman >/dev/null; then                           # Install for archlinux-based distros
   sudo pacman -S --noconfirm gcc ripgrep xsel fd python-virtualenv npm
-elif command -v dnf > /dev/null; then # Install for RHEL-based distros
+elif command -v dnf >/dev/null; then # Install for RHEL-based distros
   sudo dnf install -y gcc ripgrep xsel fd-find python3-virtualenv npm
 fi
 
 # Install for debian-based distros
-if command -v apt-get > /dev/null; then
+if command -v apt-get >/dev/null; then
   if [ "$(apt-cache show neovim | grep Version | awk -F '.' '{print $2}')" -ge 9 ]; then
     sudo apt-get install -y neovim
   else
@@ -33,12 +33,11 @@ if command -v apt-get > /dev/null; then
 fi
 
 # Install for archlinux-based distros
-if command -v pacman > /dev/null; then
+if command -v pacman >/dev/null; then
   sudo pacman -S --noconfirm neovim
 fi
 
 # Install for RHEL-based distros
-if command -v dnf > /dev/null; then
+if command -v dnf >/dev/null; then
   sudo dnf install -y neovim
 fi
-
